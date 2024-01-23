d23cb863bf04666447d41d3aca6a5ba3fa15a560
wadeddy19 <eddyw@oregonstate.edu>
2024-01-22 17:40:34 -0800

    added ya

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hello")
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('ua')
 
 
 # Press the green button in the gutter to run the script.
