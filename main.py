58bebd446cbb62d6a5d16e33323050a7281bbd47
wadeddy19 <eddyw@oregonstate.edu>
2024-01-22 17:48:09 -0800

    Revert "Add hello message"
    This reverts commit 939f3ff4da805ef2824c8f44d9b267689a30d7a5.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print("Hello")
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('ua')
 
