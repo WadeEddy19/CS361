1291f0fb705066300af4547af3b59475aed0c1a1
WadeEddy19 <eddyw@oregonstate.edu>
2024-02-12 18:19:48 -0800

    Finished Sprint 1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #CS361 Main Project
 
 def welcomePage():
+    '''Display the welcome page'''
+
     print('Welcome')
     print('This is the Home Page')
     print('''This program contains information on the previous years of the professional surfing circuit known as the WSL.
@@ -15,7 +17,7 @@ Type "help" for a detailed list of commands and how to use them.''')
 
     for i in valid_years:
         if user_input == i:
-            print('nice')
+            displayList(i)
         elif user_input == 'help' or user_input == 'Help':
             for i in range(3):
                 print()
@@ -30,6 +32,8 @@ Type "help" for a detailed list of commands and how to use them.''')
 
 #as of now only way to get to list page is from welcome screen so all back needs to do is display home screen again
 def helpPage():
+    '''Display the Help page'''
+
     print('Help')
     print()
     print('''This is the help screen, below is a list of the basic commands in the program 
@@ -70,6 +74,68 @@ This will take you to a new page with the generated list.''')
             print()
         welcomePage()
 
+
+def displayList(year):
+    '''Displays a list of surfers'''
+    file = open('C:\\List of Surfers\\' + str(year) + '.txt', 'r')
+    counter = 0
+    for current_line in file:
+        if counter == 0:
+            current_line.strip()
+            print('1: ')
+            counter += 1
+        else:
+            print(current_line.strip())
+            counter += 1
+        if counter == 2:
+            break
+
+    counter = 0
+    for current_line in file:
+       current_line.strip()
+       counter += 1
+       if counter == 2:
+           break
+    rank = 2
+    counter = 1
+    overall_counter = 0
+    for current_line in file:
+        #want 1, 2
+        if counter == 1:
+            print(rank, ': ')
+            counter += 1
+            overall_counter += 1
+            rank += 1
+        elif counter == 2:
+            print(current_line.strip())
+            counter += 1
+            overall_counter += 1
+        else:
+            counter += 1
+        if counter == 3 or counter == 4:
+            current_line.strip()
+        if counter > 4:
+            counter = 1
+
+
+        if overall_counter == 18:
+            break
+
+    print()
+    user_input = input('Type "home" to go back to the Home page: ')
+
+    if user_input == 'home':
+        for i in range(3):
+            print()
+        welcomePage()
+    elif user_input == 'exit':
+        for i in range(3):
+            print()
+        exit()
+    elif user_input == 'back':
+        for i in range(3):
+            print()
+        welcomePage()
 
 if __name__ == '__main__':
     welcomePage()
